7f706cb4b7dd7abdfc1750d0f07ba340f3c1698d
Maggie Yu <maggieyu@cmu.edu>
2017-03-29 17:31:17 -0400

    More work on form

--- application.js
+++ application.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-	
+	// Initialize the datepicker
+	$('.datepicker').pickadate({
+		selectMonths: true, 
+		selectYears: 5
+	});
+
+	// Initialize dropdowns
+	$('select').material_select();
 });
