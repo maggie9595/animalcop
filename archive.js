82152b5081aeda25f0e1aa660b5ef7b79bae879e
Maggie Yu <maggieyu@cmu.edu>
2017-04-26 12:43:55 -0400

    Cleaned up archive.js

--- archive.js
+++ archive.js
@@ -1,29 +1,7 @@
 $(document).ready(function() {
-	// Initialize the datepicker
-	$('.datepicker').pickadate({
-		selectMonths: true, 
-		selectYears: 5
-	});
-
 	// Initialize dropdowns
 	$('select').material_select();
 
 	// Initialize collapse button
   $(".button-collapse").sideNav();
-
-  // Initialize jQuery Sortable
-  $(".sortable").sortable();
-  $(".sortable").disableSelection();
-
-  // Initialize dropdown
-  $('.dropdown-button').dropdown({
-    inDuration: 300,
-    outDuration: 225,
-    constrainWidth: false, // Does not change width of dropdown to that of the activator
-    hover: false, // Activate on hover
-    gutter: 0, // Spacing from edge
-    belowOrigin: true, // Displays dropdown below the button
-    alignment: 'left', // Displays dropdown with edge aligned to the left of button
-    stopPropagation: false // Stops event propagation
-  });
 });
