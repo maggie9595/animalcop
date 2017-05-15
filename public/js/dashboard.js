eccc9aee51faa9e38700558188f60187ed908c6c
Maggie Yu <maggieyu@cmu.edu>
2017-05-15 05:26:14 -0400

    Itinerary formatting

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -41,67 +41,35 @@ function removeFromItinerary(id) {
 
 // Google Maps
 function initMap() {
-  console.log(itinerary);
-
-  var locations = [];
-
-  for (var i=0; i < itinerary.length; i++) {
-    locations.push([itinerary[i].address, itinerary[i].latitude, itinerary[i].longitude, 0]);
-  }
-
-  // var broadway = {
-  //   info: '<strong>Chipotle on Broadway</strong><br>\
-  //         5224 N Broadway St<br> Chicago, IL 60640<br>\
-  //         <a href="https://goo.gl/maps/jKNEDz4SyyH2">Get Directions</a>',
-  //   lat: 41.976816,
-  //   long: -87.659916
-  // };
-
-  // var belmont = {
-  //   info: '<strong>Chipotle on Belmont</strong><br>\
-  //         1025 W Belmont Ave<br> Chicago, IL 60657<br>\
-  //         <a href="https://goo.gl/maps/PHfsWTvgKa92">Get Directions</a>',
-  //   lat: 41.939670,
-  //   long: -87.655167
-  // };
-
-  // var sheridan = {
-  //   info: '<strong>Chipotle on Sheridan</strong><br>\r\
-  //         6600 N Sheridan Rd<br> Chicago, IL 60626<br>\
-  //         <a href="https://goo.gl/maps/QGUrqZPsYp92">Get Directions</a>',
-  //   lat: 42.002707,
-  //   long: -87.661236
-  // };
-
-  // var locations = [
-  //     [broadway.info, broadway.lat, broadway.long, 0],
-  //     [belmont.info, belmont.lat, belmont.long, 1],
-  //     [sheridan.info, sheridan.lat, sheridan.long, 2],
-  //   ];
-
  var map = new google.maps.Map(document.getElementById('ho-map'), {
     center: {lat: 40.440624, lng: -79.995888},
     zoom: 13
   });
 
   var marker = new google.maps.Marker({
-    map: map,
-    position: {lat: 40.440624, lng: -79.995888}
+    map: map
   });
 
-  for (var i = 0; i < locations.length; i++) {
+  var itineraryLocations = [];
+
+  for (var i=0; i < itinerary.length; i++) {
+    itineraryLocations.push(["In your itinerary:<br><br>" + itinerary[i].address + "<br><br> Received on " + itinerary[i].received, itinerary[i].latitude, itinerary[i].longitude, 0]);
+  }
+
+  for (var i = 0; i < itineraryLocations.length; i++) {
     marker = new google.maps.Marker({
-      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+      position: new google.maps.LatLng(itineraryLocations[i][1], itineraryLocations[i][2]),
       map: map
     });
 
     google.maps.event.addListener(marker, 'click', (function (marker, i) {
       return function () {
-        infowindow.setContent(locations[i][0]);
+        infowindow.setContent(itineraryLocations[i][0]);
         infowindow.open(map, marker);
       }
     })(marker, i));
   }
+
   
   var infowindow = new google.maps.InfoWindow();
   var infowindowContent = document.getElementById('infowindow-content');
